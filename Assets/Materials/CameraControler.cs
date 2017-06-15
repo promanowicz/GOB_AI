188a17c5fc4d07f34247046a086616622690cf31
promanowicz <roman1556@o2.pl>
2017-06-15 15:40:02 +0200

    baisc scene

---
+++ Assets/Materials/CameraControler.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class CameraControler : MonoBehaviour{
+    private Camera mCamera;
+    public String textDscr = "Is Camera Moving: ";
+    public float cameraMoveMargin = 0.1f;
+    public float maxCameraSpeed = 0.5f;
+    private float screenWidth;
+    private float screenHeight;
+    public bool isMovementEnabled = true;
+
+    void Start(){
+        mCamera = GetComponent<Camera>();
+        screenWidth = Screen.width * cameraMoveMargin;
+        screenHeight = Screen.height * cameraMoveMargin;
+    }
+
+    void Update(){
+        if (Input.GetKeyDown(KeyCode.Space)){
+            isMovementEnabled = !isMovementEnabled;
+        }
+        if (isMovementEnabled){
+            Vector3 mPos = Input.mousePosition;
+            Vector3 cameraPos = transform.position;
+            float flowSpeed = 1f;
+            if (mPos.x < screenWidth){
+                flowSpeed = (screenWidth - mPos.x) / 100;
+                cameraPos.x -= flowSpeed * maxCameraSpeed;
+            }
+            if (mPos.x > Screen.width - screenWidth){
+                float w = Screen.width;
+                w = w - (w - screenWidth);
+                float progress = mPos.x - (Screen.width - screenWidth);
+                flowSpeed = progress / w;
+                cameraPos.x += flowSpeed * maxCameraSpeed;
+            }
+            if (mPos.y < screenHeight){
+                flowSpeed = (screenHeight - mPos.y) / 100;
+                cameraPos.z -= flowSpeed * maxCameraSpeed;
+            }
+            if (mPos.y > Screen.height - screenHeight){
+                cameraPos.z += flowSpeed * maxCameraSpeed;
+            }
+            transform.position = cameraPos;
+        }
+    }
+
+    void p(object msg){
+        Debug.Log(msg);
+    }
+}
---
+++ Assets/Materials/PlayerController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour{
+    public Camera playerCamera;
+
+    public float playerSpeed;
+
+/*    public Text hpText;
+    public Text pointsText;
+    public Text TimeLeftText;*/
+
+    public string HPpointsDscr = "HP: ";
+
+    private Vector3 goToPoint;
+
+    private Rigidbody mRigidbody;
+
+    public int hpPoints = 100;
+
+    private bool hasGoToPos = false;
+
+    void Start(){
+        goToPoint = transform.position;
+        mRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    private bool isFiring = false;
+    void Update(){
+        Vector3 positon = Input.mousePosition;
+        positon.z = playerCamera.transform.position.y;
+        goToPoint = playerCamera.ScreenToWorldPoint(positon);
+        goToPoint.y = transform.position.y;
+        if (Input.GetButtonDown("Fire1")){
+            transform.forward = goToPoint;
+            hasGoToPos = true;
+        }
+        if (Input.GetButtonDown("Fire2")){
+            isFiring = true;
+        }
+        if(Input.GetButtonUp("Fire2")){
+            isFiring = false;
+        }
+        if (isFiring){
+            transform.LookAt(goToPoint);
+        }
+    }
+
+    private void FixedUpdate(){
+        if (isPositionReached()){
+            hasGoToPos = false;
+            mRigidbody.velocity = Vector3.zero;
+        }
+        else{
+            if (hasGoToPos){
+                transform.LookAt(goToPoint);
+                Vector3 velocity = (goToPoint - transform.position).normalized * playerSpeed;
+                if (!velocity.Equals(mRigidbody.velocity))
+                    mRigidbody.velocity = velocity;
+            }
+        }
+    }
+
+    bool isPositionReached(){
+        int dstX = Mathf.RoundToInt(goToPoint.x);
+        int dstZ = Mathf.RoundToInt(goToPoint.z);
+        int currX = Mathf.RoundToInt(transform.position.x);
+        int currY = Mathf.RoundToInt(transform.position.z);
+        return dstX == currX && dstZ == currY;
+    }
+
+/*    private void OnCollisionEnter(Collision other){
+        if (other.gameObject.tag == "Trap" || other.gameObject.tag=="Enemy"){
+            Vector3 otherPos = other.gameObject.transform.position;
+            otherPos -= transform.position;
+            //otherPos.y = transform.position.y;
+            hasGoToPos = false;
+            mRigidbody.velocity = Vector3.zero;
+            mRigidbody.AddForce(otherPos.normalized * -30);
+            hpPoints -= 1;
+        }
+    }*/
+
+}
