84acc91ca0de6518b845d6f3eaf6322989f616e8
promanowicz <roman1556@o2.pl>
2017-06-16 18:54:26 +0200

    addedbullet

---
+++ Assets/Bullet.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour{
+
+    public Transform debugTransform;
+
+    public float timeToLiveSeconds = 4;
+    public float speed = 3;
+    public bool isFired = false;
+
+    private Rigidbody mRigidbody;
+
+    void Start()
+    {
+        mRigidbody = GetComponent<Rigidbody>();
+        fireAt(debugTransform);
+    }
+
+    public void fireAt(Transform t){
+        transform.LookAt(t);
+        mRigidbody.velocity = (t.position - transform.position).normalized * speed;
+        isFired = true;
+    }
+
+	void Update () {
+	    if (isFired){
+	        timeToLiveSeconds -= Time.deltaTime;
+	        if (timeToLiveSeconds < 0){
+	            Destroy(gameObject);
+	        }
+	    }
+	}
+
+    private void OnCollisionEnter(Collision col){
+        Destroy(gameObject);
+    }
+}
